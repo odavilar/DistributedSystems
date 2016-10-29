19531940d17cb4ff354116a2afa7f18798284e2c
Oscar Davila <odavilar@gmail.com>
2016-10-29 00:17:29 -0500

    Server sending index.html

--- HttpServer/src/httpserver/HttpServer.java
+++ HttpServer/src/httpserver/HttpServer.java
@@ -7,6 +7,9 @@ package httpserver;
 
 import java.net.*;
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.io.FileUtils;
 
 /**
  *
@@ -29,11 +32,39 @@ public class HttpServer {
             // on reste bloqué sur l'attente d'une demande client
             Socket clientSocket = serverSocket.accept();
             System.err.println("Nouveau client connecté");
+            ServerConnection cNewConnection = new ServerConnection(clientSocket);
 
-            // on ouvre un flux de converation
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        }
+    }
+}
 
+class ServerConnection extends Thread {
+
+    BufferedReader in;
+    BufferedWriter out;
+    DataInputStream htmlFile;
+    BufferedInputStream BufferedInput;
+    String sFileName = new String("index.html");
+    Socket clientSocket;
+
+    public ServerConnection(Socket aClientSocket) {
+            clientSocket = aClientSocket;
+            try {
+                // on ouvre un flux de converation
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+            } catch (IOException ex) {
+                Logger.getLogger(ServerConnection.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            System.out.println("New client");
+            
+            this.start();
+    }
+
+    public void run() {
+
+        try {
             // chaque fois qu'une donnée est lue sur le réseau on la renvoi sur
             // le flux d'écriture.
             // la donnée lue est donc retournée exactement au même client.
@@ -44,23 +75,27 @@ public class HttpServer {
                     break;
                 }
             }
-
-            out.write("HTTP/1.0 200 OK\r\n");
+            File fFile = new File("index.html");
+            String sFile = FileUtils.readFileToString(fFile,"UTF-8");
+            
+            out.write("HTTP/1.1 200 OK\r\n");
             out.write("Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n");
             out.write("Server: Apache/0.8.4\r\n");
             out.write("Content-Type: text/html\r\n");
-            out.write("Content-Length: 57\r\n");
+
+            out.write("Content-Length: " + sFile.length() + "\r\n");
             out.write("Expires: Sat, 01 Jan 2000 00:59:59 GMT\r\n");
             out.write("Last-modified: Fri, 09 Aug 1996 14:21:40 GMT\r\n");
             out.write("\r\n");
-            out.write("<TITLE>Exemple</TITLE>");
-            out.write("<P>Ceci est une page d'exemple.</P>");
-
+            out.write(sFile);
             // on ferme les flux.
             System.err.println("Connexion avec le client terminée");
             out.close();
             in.close();
             clientSocket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ServerConnection.class.getName()).log(Level.SEVERE, null, ex);
         }
+
     }
 }
