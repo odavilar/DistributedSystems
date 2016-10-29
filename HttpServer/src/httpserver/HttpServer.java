43a9e0eaaf23b40b52e1b56b17a657dc7f0e2ab2
Oscar Davila <odavilar@gmail.com>
2016-10-29 17:11:43 -0500

    Char approach

--- HttpServer/src/httpserver/HttpServer.java
+++ HttpServer/src/httpserver/HttpServer.java
@@ -42,7 +42,6 @@ public class HttpServer {
 class ServerConnection extends Thread {
 
     DataInputStream htmlFile;
-    BufferedInputStream BufferedInput;
     Socket clientSocket;
     HttpRequestHandler cHttpRequestHdlr;
 
@@ -61,71 +60,84 @@ class HttpRequestHandler {
 
     BufferedReader in;
     BufferedWriter out;
+    BufferedInputStream inBuf;
     Hashtable<String, String> cHeader = new Hashtable<String, String>();
     String sReqType;
     String sReqVersion;
     String sUrl;
 
-    HttpRequestHandler(Socket clientSocket) throws IOException {
+    HttpRequestHandler(Socket clientSocket) {
         try {
             // on ouvre un flux de converation
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            inBuf = new BufferedInputStream(new ObjectInputStream(clientSocket.getInputStream()));
             out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-        } catch (IOException ex) {
-            Logger.getLogger(ServerConnection.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        /* Get request type, url and version. */
-        String s;
-        s = in.readLine();
-        System.out.println(s);
-        String[] sReq = s.split(" ");
-        sReqType = sReq[0];
-        sUrl = sReq[1];
-        sReqVersion = sReq[2];
-        while ((s = in.readLine()) != null) {
+            /* Get request type, url and version. */
+            String s;
+            s = in.readLine();
             System.out.println(s);
-            if (!s.isEmpty()) {
-                cHeader.put(s.substring(0, s.indexOf(":")), s.substring(s.indexOf(":") + 2, s.length()));
-            } else {
-                break;
-            }
-        }
-
-        if (sReqType.equals("POST")) {
-            int i = 0;
-            int ctr = 0;
-            char c;
-            int iLength = Integer.parseInt(cHeader.get("Content-Length"));
-            System.out.println("Body Length: " + iLength);
-            while ((i = in.read()) != -1) {
-                // int to character
-                c = (char) i;
-                // print char
-                System.out.println("Character Read: " + c + " " + ++ctr);
-                if(ctr == iLength)
-                {
+            String[] sReq = s.split(" ");
+            sReqType = sReq[0];
+            sUrl = sReq[1];
+            sReqVersion = sReq[2];
+            while ((s = in.readLine()) != null) {
+                System.out.println(s);
+                if (!s.isEmpty()) {
+                    cHeader.put(s.substring(0, s.indexOf(":")), s.substring(s.indexOf(":") + 2, s.length()));
+                } else {
                     break;
                 }
             }
+
+            if (sReqType.equals("POST")) {
+                int i = 0;
+                int ctr = 0;
+                char c;
+                int iLength = Integer.parseInt(cHeader.get("Content-Length"));
+                StringBuilder cString = new StringBuilder();
+                System.out.println("Body Length: " + iLength);
+                if (!cHeader.get("Content-Type").contains("multipart/form-data")) {
+                    while ((i = in.read()) != -1) {
+                        // int to character
+                        c = (char) i;
+                        cString.append(c);
+                        // print char
+                        if (++ctr == iLength) {
+                            break;
+                        }
+                    }
+                    System.out.println(cString.toString());
+                } else {
+                    byte[] bFileInBytes = new byte[iLength];
+                    inBuf.read(bFileInBytes, 0, iLength);
+                    FileOutputStream fileoutputstream = new FileOutputStream("outputfile");
+                    BufferedOutputStream bufferedoutput = new BufferedOutputStream(fileoutputstream);
+                    bufferedoutput.write(bFileInBytes, 0, bFileInBytes.length);
+                    bufferedoutput.flush();
+                }
+            }
+
+            File fFile = new File("src/index.html");
+            String sFile = FileUtils.readFileToString(fFile, "UTF-8");
+
+            out.write("HTTP/1.1 200 OK\r\n");
+            out.write("Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n");
+            out.write("Server: Apache/0.8.4\r\n");
+            out.write("Content-Type: text/html\r\n");
+            out.write("Content-Length: " + sFile.length() + "\r\n");
+            out.write("Expires: Sat, 01 Jan 2000 00:59:59 GMT\r\n");
+            out.write("Last-modified: Fri, 09 Aug 1996 14:21:40 GMT\r\n");
+            out.write("\r\n");
+            out.write(sFile);
+            // on ferme les flux.
+            System.err.println("Connexion avec le client terminée");
+            out.close();
+            in.close();
+            clientSocket.close();
+        } catch (IOException ex) {
+            System.err.println("Exception");
+            Logger.getLogger(ServerConnection.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        File fFile = new File("src/index.html");
-        String sFile = FileUtils.readFileToString(fFile, "UTF-8");
-
-        out.write("HTTP/1.1 200 OK\r\n");
-        out.write("Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n");
-        out.write("Server: Apache/0.8.4\r\n");
-        out.write("Content-Type: text/html\r\n");
-        out.write("Content-Length: " + sFile.length() + "\r\n");
-        out.write("Expires: Sat, 01 Jan 2000 00:59:59 GMT\r\n");
-        out.write("Last-modified: Fri, 09 Aug 1996 14:21:40 GMT\r\n");
-        out.write("\r\n");
-        out.write(sFile);
-        // on ferme les flux.
-        System.err.println("Connexion avec le client terminée");
-        out.close();
-        in.close();
-        clientSocket.close();
     }
 
 }
